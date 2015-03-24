67386a6a6d5b91e8417ad561340a1f55e2c3a8c5
peta92 <aless.petrillo@gmail.com>
2015-03-24 23:42:31 +0100

    works

--- src/main/WaitThread.java
+++ src/main/WaitThread.java
@@ -1,8 +1,8 @@
 package main;
 
+import java.io.IOException;
 
-
-import java.io.DataInputStream;
+import javax.bluetooth.BluetoothStateException;
 import javax.bluetooth.DiscoveryAgent;
 import javax.bluetooth.LocalDevice;
 import javax.bluetooth.UUID;
@@ -11,19 +11,9 @@ import javax.microedition.io.StreamConnection;
 import javax.microedition.io.StreamConnectionNotifier;
 
 public class WaitThread implements Runnable{
-    
-    public final UUID uuid = new UUID("41428440cf0211e488300800200c9a66", false); //it can be generated randomly
-    public final String name = "PREN30BlueServer";                  //the name of the service
-    public final String url  =  "btspp://localhost:" + uuid         //the service url
-                                + ";name=" + name 
-                                + ";authenticate=false;encrypt=false;";
-    LocalDevice local = null;
-    StreamConnectionNotifier server = null;
-    StreamConnection conn = null;
-    
+
 	/** Constructor */
 	public WaitThread() {
-            System.out.println(uuid);
 	}
 	
 	@Override
@@ -32,26 +22,45 @@ public class WaitThread implements Runnable{
 	}
 	
 	/** Waiting for connection from devices */
-	 public void waitForConnection() {
-        try {
-            System.out.println("Setting device to be discoverable...");
-            local = LocalDevice.getLocalDevice();
-            local.setDiscoverable(DiscoveryAgent.GIAC);
-            System.out.println("Start advertising service...");
-            server = (StreamConnectionNotifier)Connector.open(url);
-            System.out.println("Waiting for incoming connection...");
-            conn = server.acceptAndOpen();
-            System.out.println("Client Connected...");
-            DataInputStream din   = new DataInputStream(conn.openInputStream());
-            while(true){
-                String cmd = "";
-                char c;
-                while (((c = din.readChar()) > 0) && (c!='\n') ){
-                    cmd = cmd + c;
-                }
-                System.out.println("Received " + cmd);
-            }
-            
-        } catch (Exception  e) {System.out.println("Exception Occured: " + e.toString());}
-    }
+	private void waitForConnection() {
+		// retrieve the local Bluetooth device object
+		LocalDevice local = null;
+		
+		StreamConnectionNotifier notifier;
+		StreamConnection connection = null;
+		
+		// setup the server to listen for connection
+		try {
+			local = LocalDevice.getLocalDevice();
+			local.setDiscoverable(DiscoveryAgent.GIAC);
+			
+			UUID uuid = new UUID("04c6093b00001000800000805f9b34fb", false);
+			System.out.println(uuid.toString());
+			
+            String url = "btspp://localhost:" + uuid.toString() + ";name=RemoteBluetooth";
+            notifier = (StreamConnectionNotifier)Connector.open(url);
+        } catch (BluetoothStateException e) {
+        	System.out.println("Bluetooth is not turned on.");
+			e.printStackTrace();
+			return;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		// waiting for connection
+		while(true) {
+			try {
+				System.out.println("waiting for connection...");
+	            connection = notifier.acceptAndOpen();
+	            
+	            Thread processThread = new Thread(new ProcessConnectionThread(connection));
+	            processThread.start();
+	            
+			} catch (Exception e) {
+				e.printStackTrace();
+				return;
+			}
+		}
+	}
 }
